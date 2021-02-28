e3f696f17b19bbb6efa62d2dd4d42cf238f86b5d
SileceInside <zubr_artur@bk.ru>
2021-02-28 19:01:08 +0300

    init

--- crypto/settings.py
+++ crypto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2vh8zi#+d9-#1n99pv^ad18#m*m$fp7%a&7s6*1^k_=18b)xq@'
+from settings_local import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
